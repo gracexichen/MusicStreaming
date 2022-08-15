df9b5161ef11950a650e5c2f621c95b60bdaa4f0
Grace Chen <gracechen56607@gmail.com>
2022-08-14 22:08:45 -0700

    add authentication system

--- music/urls.py
+++ music/urls.py
@@ -3,10 +3,14 @@ from starmusic import settings
 from django.conf.urls.static import static
 from . import views
 
-app_name = "music"
 urlpatterns = [
+    #main webpage
     path('', views.about, name='about'),
     path('about', views.about, name='about'),
     path('support', views.support, name='support'),
     path('browse', views.browse, name='browse'),
+    #authentication
+    path('login', views.login_view, name="login"),
+    path('home', views.home, name="home"),
+    path('logout', views.logout_view, name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- music/views.py
+++ music/views.py
@@ -1,7 +1,12 @@
+from curses.ascii import HT
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Songs
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+#main webpage
 def index(request):
     return render(request, "music/index.html")
 
@@ -17,3 +22,28 @@ def browse(request):
         "songs": songs,
     })
 
+#authentication
+def home(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "music/user.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "music/login.html", {
+                "message": "Invalid credentials."
+            })
+    return render(request, "music/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "music/login.html", {
+        "message": "Logged out."
+    })
