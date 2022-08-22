da492200366397422e840f522d4b0fba148e4668
Grace Chen <gracechen56607@gmail.com>
2022-08-22 11:05:55 -0700

    commit all

--- music/views.py
+++ music/views.py
@@ -35,7 +35,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("home"))
+            return HttpResponseRedirect(reverse("browse"))
         else:
             return render(request, "music/login.html", {
                 "message": "Invalid credentials."
